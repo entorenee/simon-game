acd72a078405f311f4c83b3a96b68b142ca670b5
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-08-01 22:44:53 -0700

    Convert to functional component

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,50 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import '../style/Counter.css';
 
-class Counter extends Component {
-  shouldComponentUpdate(nextProps) {
-    const { gameOn, moveCount } = this.props;
-    if (moveCount !== nextProps.moveCount || gameOn !== nextProps.gameOn) {
-      return true;
-    }
-    return false;
+const Counter = props => {
+  const { gameOn, moveCount } = props;
+  const moveCountString = moveCount < 10 ? `0${moveCount}` : moveCount;
+  let moveCountDisplay;
+  if (!gameOn) {
+    moveCountDisplay = '';
+  } else {
+    moveCountDisplay = moveCount !== 0 ? moveCountString : '--';
   }
 
-  render() {
-    const { gameOn, moveCount } = this.props;
-    const moveCountString = moveCount < 10 ? `0${moveCount}` : moveCount;
-    let moveCountDisplay;
-    if (!gameOn) {
-      moveCountDisplay = '';
-    } else {
-      moveCountDisplay = moveCount !== 0 ? moveCountString : '--';
-    }
-
-    if (moveCountDisplay === '! !' || moveCountDisplay === 'WIN!') {
-      const ele = this.currentCount;
-      ele.classList.add('counter-blink');
-      setTimeout(() => {
-        ele.classList.remove('counter-blink');
-      }, 1700);
-    }
-    return (
-      <div className="counter-wrap">
-        <div className="counter-display">
-          <span
-            className="current-count"
-            ref={input => {
-              this.currentCount = input;
-            }}
-          >
-            {moveCountDisplay}
-          </span>
-        </div>
-        <span className="count-label">COUNT</span>
+  if (moveCountDisplay === '! !' || moveCountDisplay === 'WIN!') {
+    const ele = document.querySelector('.current-count');
+    ele.classList.add('counter-blink');
+    setTimeout(() => {
+      ele.classList.remove('counter-blink');
+    }, 1700);
+  }
+  return (
+    <div className="counter-wrap">
+      <div className="counter-display">
+        <span className="current-count">{moveCountDisplay}</span>
       </div>
-    );
-  }
-}
+      <span className="count-label">COUNT</span>
+    </div>
+  );
+};
 
 Counter.propTypes = {
   moveCount: PropTypes.string.isRequired,
