ffc6becd013e9bc53a749c8afbda319e72de0157
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-10-19 08:24:24 -0700

    Remove use of gulp

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var projectFolder = '../../../portfolio_site/projects/simon-game';
-
-gulp.task('copy', function() {
-  return gulp.src('./build/**/*')
-             .pipe(gulp.dest(projectFolder))
-});
